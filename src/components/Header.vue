2046a6d5c5aa9124d6e7ba5386ab019dabacc795
lucaperini <luca.perini@gmail.com>
2022-04-07 19:05:48 +0200

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,44 +1,38 @@
 <template>
     <header>
-    
-      <nav class="navbar navbar-expand-lg">
+        <nav class="navbar navbar-expand-lg">
         <div class="container">
-        
-        <a class="navbar-brand" href="#">
-        <img class="main-logo" src="../assets/images/logo-2x.png" alt="">
-        </a>
-        <div  id="navbarSupportedContent">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0 fs-5">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Services</a>
-        </li>
-        
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Videos</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Blog</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Store</a>
-        </li>
-      </ul>
-    </div>
-  </div>
-
-</nav>
-
-
-
-
-    
-  </header>
+            <a class="navbar-brand" href="#">
+            <img class="main-logo" src="../assets/images/logo-2x.png" alt="">
+            </a>
+            <div id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0 fs-5">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Services</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Videos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Store</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="btn-sm new">new</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        </nav>
+    </header>
 </template>
 
 <script>
@@ -67,6 +61,25 @@ export default {
     
     .main-logo {
         width:50%;
+        
+    }
+
+  .new {
+        background-color: rgb(255, 249, 102);
+        font-size:.8rem;
+        width:40px;
+        height:30px;
+        color:black;
+        text-decoration: none;
+        line-height: 3rem;
+        
+        
+        
+       
+        
+        
+
+        
         
     }
 </style>
